ded8a9aa6075ba8757ebdc49950815921e943bb6
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-12-21 11:42:40 -0500

    Remove Twitter and YouTube icons from FooterSocial component

--- app/components/FooterSocial/FooterSocial.tsx
+++ app/components/FooterSocial/FooterSocial.tsx
@@ -10,12 +10,6 @@ export function FooterSocial() {
         <Image src='/swingingsam.png' width={29} height={50} alt="Swinging Sam" />
         <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
           <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
             <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
           </ActionIcon>
         </Group>
