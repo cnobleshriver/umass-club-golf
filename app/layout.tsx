47749f25f11e130476d7ef81480be10b2482c2fc
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-12-23 18:31:40 -0500

    Update metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
 
 export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
+  title: "UMass Club Golf",
+  description: "NCCGA Club Golf Team at UMass Amherst",
 };
 
 export default function RootLayout({ children }: { children: any }) {
