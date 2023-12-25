84e5abf13e17db0dedc2c4298c6c8a7275ecc770
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-12-24 23:24:25 -0500

    Remove commented out code in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-// import { DoubleHeader } from "./components/DoubleHeader/DoubleHeader";
-// import { FooterSocial } from "./components/FooterSocial/FooterSocial"
 import { Hero } from "./components/Hero/Hero";
 
 export default function HomePage() {
   return <>
-    {/* <DoubleHeader /> */}
     <Hero />
-    {/* <FooterSocial /> */}
   </>;
 }
