7362075388118ba93279a0e435705fc4cce403b3
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-12-26 14:42:06 -0500

    implement burger dropdown

--- src/components/DoubleHeader/DoubleHeader.tsx
+++ src/components/DoubleHeader/DoubleHeader.tsx
@@ -1,22 +1,20 @@
-'use client';
+"use client";
 
 import "@mantine/core/styles.css";
-import { useState } from 'react';
-import { Container, Group, Burger, Box } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import classes from './DoubleHeader.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState } from "react";
+import { Container, Group, Burger, Box, Menu } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import classes from "./DoubleHeader.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-const userLinks = [
-  { link: '#', label: 'Spring \'24 Calendar' },
-];
+const userLinks = [{ link: "#", label: "Spring '24 Calendar" }];
 
 const mainLinks = [
-  { link: '/', label: 'Home' },
-  { link: '/roster', label: 'Roster' },
-  { link: '#', label: 'Tournament Results' },
-  { link: '#', label: 'Contact' },
+  { link: "/", label: "Home" },
+  { link: "/roster", label: "Roster" },
+  { link: "#", label: "Tournament Results" },
+  { link: "#", label: "Contact" },
 ];
 
 export function DoubleHeader() {
@@ -24,9 +22,9 @@ export function DoubleHeader() {
   const [active, setActive] = useState(0);
 
   const mainItems = mainLinks.map((item, index) => (
-    <Link 
-      href={item.link} 
-      key={item.label} 
+    <Link
+      href={item.link}
+      key={item.label}
       className={classes.mainLink}
       data-active={index === active || undefined}
       onClick={() => setActive(index)}
@@ -36,11 +34,7 @@ export function DoubleHeader() {
   ));
 
   const secondaryItems = userLinks.map((item) => (
-    <Link
-      href={item.link}
-      key={item.label}
-      className={classes.secondaryLink}
-    >
+    <Link href={item.link} key={item.label} className={classes.secondaryLink}>
       {item.label}
     </Link>
   ));
@@ -48,20 +42,34 @@ export function DoubleHeader() {
   return (
     <header className={classes.header}>
       <Container className={classes.inner}>
-        <Image src='/umasslogo.png' width={120} height={40} alt="UMass Club Golf" />
+        <Image
+          src="/umasslogo.png"
+          width={120}
+          height={40}
+          alt="UMass Club Golf"
+        />
         <Box className={classes.links} visibleFrom="sm">
           <Group justify="flex-end">{secondaryItems}</Group>
           <Group gap={0} justify="flex-end" className={classes.mainLinks}>
             {mainItems}
           </Group>
         </Box>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          className={classes.burger}
-          size="sm"
-          hiddenFrom="sm"
-        />
+        <Menu opened={opened} onClose={toggle}>
+          <Menu.Target>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              size="sm"
+              hiddenFrom="sm"
+            />
+          </Menu.Target>
+          <Menu.Dropdown>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              {secondaryItems}
+              {mainItems}
+            </div>
+          </Menu.Dropdown>
+        </Menu>
       </Container>
     </header>
   );
