5c57e571222b0b20076f778be299e63674e87565
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-01-06 10:30:56 -0500

    Add contact information to ContactPage component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,19 @@
+import styles from './ContactPage.module.css';
 
 export default function ContactPage() {
   return (
-    <>
-        <h1>Contact Page</h1>
-    </>
+    <div className={styles.container}>
+      <h1>Contact Us</h1>
+      <div className={styles.contactCard}>
+        <h2>Club President</h2>
+        <p>Sam Robert</p>
+        <p><a href="mailto:samuelrobert@umass.edu">samuelrobert@umass.edu</a></p>
+      </div>
+      <div className={styles.contactCard}>
+        <h2>Treasurer</h2>
+        <p>Mike Pietrini</p>
+        <p><a href="mailto:mpietrini@umass.edu">mpietrini@umass.edu</a></p>
+      </div>
+    </div>
   );
 }
