d6110d5d159836c7d72dae0461e6c29cf4a390df
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-01-06 11:14:11 -0500

    replace ' with &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
         <h2>About Us</h2>
         <div className={styles.aboutText}>
           <p>
-            Chartered in 1987 at the University of Massachusetts, we've grown
+            Chartered in 1987 at the University of Massachusetts, we&apos;ve grown
             from a small group of golfing students to a prominent club known for
             our sportsmanship, teamwork, and dedication to promoting golf within
             the university. Our team currently participates in the National
