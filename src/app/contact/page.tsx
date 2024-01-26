03d922d7df7565bf37943eb6dfc023fa00d1a0b3
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-01-26 12:37:50 -0500

    Add interest form to ContactPage module

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,19 +2,19 @@ import styles from "./ContactPage.module.css";
 
 const contacts = [
   {
-    title: 'President',
-    name: 'Sam Robert',
-    email: 'samuelrobert@umass.edu'
+    title: "President",
+    name: "Sam Robert",
+    email: "samuelrobert@umass.edu",
   },
   {
-    title: 'Vice President',
-    name: 'Chris Pietrini',
-    email: 'cpietrini@umass.edu'
+    title: "Vice President",
+    name: "Chris Pietrini",
+    email: "cpietrini@umass.edu",
   },
   {
-    title: 'Treasurer',
-    name: 'Mike Pietrini',
-    email: 'mpietrini@umass.edu'
+    title: "Treasurer",
+    name: "Mike Pietrini",
+    email: "mpietrini@umass.edu",
   },
 ];
 
@@ -22,6 +22,14 @@ export default function ContactPage() {
   return (
     <div className={styles.container}>
       <h1>Contact Us</h1>
+      <div className={styles.interestForm}>
+        <p>
+          Interested in joining us? Fill out this{" "}
+          <a href="" target="_blank" rel="noopener noreferrer">
+            Interest Form
+          </a>
+        </p>
+      </div>
       {contacts.map((contact, index) => (
         <div key={index} className={styles.contactCard}>
           <h2>{contact.title}</h2>
