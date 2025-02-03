634cf0622a6ce1d7a391cae23a3ee8b7d37857eb
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2025-02-03 18:57:22 -0500

    update instagram link

--- src/components/FooterSocial/FooterSocial.tsx
+++ src/components/FooterSocial/FooterSocial.tsx
@@ -9,7 +9,7 @@ export function FooterSocial() {
       <Container className={classes.inner}>
         <Image src='/swingingsam.png' width={120} height={60} alt="UMass Club Golf" className={classes.logoImage} />
         <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
-          <ActionIcon component="a" href='https://www.instagram.com/umassclubgolf/' size="lg" color="gray" variant="subtle">
+          <ActionIcon component="a" href='https://www.instagram.com/umass_golf/' size="lg" color="gray" variant="subtle">
             <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
           </ActionIcon>
         </Group>
